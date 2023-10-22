752d19e1b76d5d37c31fb9e39010f004b9089e66
hannahrajarao <hannahrajarao@gmail.com>
2023-10-22 16:49:11 -0400

    Initial script functions

---
+++ script.js
@@ -0,0 +1,67 @@
+const timeRemainingEl = document.getElementById("time-remaining");
+const scoreEl = document.getElementById("score");
+
+const numberOfHoles = 7;
+var chosenHole;
+var timeRemaining = 60;
+var score = 0;
+var gameInProgress = false;
+
+function startGame() {
+    gameInProgress = true;
+    score = 0;
+    updateScore();
+    startTimer();
+    chooseHole();
+}
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        if(timeRemaining <=0) {
+            clearInterval(timerInterval);
+            gameInProgress = false;
+        }
+        else {
+            timeRemaining--;
+            updateTimeRemaining();
+        }
+    }, 1000);
+}
+
+function selectHole(holeNumber) {
+    if(gameInProgress) {
+        if(holeNumber == chosenHole) {
+            score++;
+            updateScore();
+            turnOffLight(chosenHole);
+            chooseHole();
+        }
+        else {
+            score--;
+            updateScore();
+        }
+    }
+    
+}
+
+function chooseHole() {
+    chosenHole = Math.floor(Math.random()*numberOfHoles);
+    console.log(chosenHole);
+    turnOnLight(chosenHole);
+}
+
+function turnOnLight(holeNum) {
+    document.getElementById(holeNum).style.backgroundColor = 'lime';
+}
+
+function turnOffLight(holeNum) {
+    document.getElementById(holeNum).style.backgroundColor = 'green';
+}
+
+function updateTimeRemaining() {
+    timeRemainingEl.innerHTML = timeRemaining;
+}
+
+function updateScore() {
+    scoreEl.innerHTML = score;
+}
